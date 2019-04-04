d948f85b899de6610ec0162ceca0d7c92bb50b5e
Anna <ioanaavram46@yahoo.com>
2019-04-04 14:45:17 +0100

    fixed the video page, was crashing app

--- EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/MainActivity.java
+++ EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/MainActivity.java
@@ -50,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, contact_page.class));
             }
         });
+        Button videoButton= (Button) findViewById(R.id.VideoLessonsBtn);
+        videoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                startActivity(new Intent(MainActivity.this, VideoActivity.class));
+            }
+        });
 
         Button revisionButton= (Button) findViewById(R.id.TestQuestionsBtn);
         revisionButton.setOnClickListener(new View.OnClickListener(){
