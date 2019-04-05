2554839354a52b8dfa96987b6f5b7519129e7acb
Anna <ioanaavram46@yahoo.com>
2019-04-05 16:12:00 +0100

    The video page works Sample video for now taken off youtube

---
+++ EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/Config.java
@@ -0,0 +1,9 @@
+package com.example.easylearndrivinglessons;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "\n" + "AIzaSyAetj7N9qEOj2DF02Vg1f10U0iXbUYZgn0\n";
+}
--- EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/VideoActivity.java
+++ EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/VideoActivity.java
@@ -1,55 +1,64 @@
 package com.example.easylearndrivinglessons;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.URLUtil;
 import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        video1Test = findViewById(R.id.videoView);
-        MediaController controller = new MediaController(this);
-        controller.setMediaPlayer(video1Test);
-        video1Test.setMediaController(controller);
-    }
-    private static final String VIDEO_SAMPLE = "tacoma_narrows";
-    private VideoView video1Test;
 
-    private Uri getMedia(String mediaName) {
-        return Uri.parse("android.resource://" + getPackageName() +
-                "/raw/" + mediaName);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
     }
-    private void initializePlayer() {
-        Uri videoUri = getMedia(VIDEO_SAMPLE);
-        video1Test.setVideoURI(videoUri);
-    }
-    private void releasePlayer() {
-        video1Test.stopPlayback();
-    }
+
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        initializePlayer();
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        releasePlayer();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            video1Test.pause();
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("lhYBX9aoIdY"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
         }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
