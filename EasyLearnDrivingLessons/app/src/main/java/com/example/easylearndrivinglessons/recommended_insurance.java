fdf045165535480e678a634d36354e52e31c4adc
HannahNCI <x18100325@student.ncirl.ie>
2019-04-09 11:17:47 +0100

    I have added the title bar for the recommended insurance page and recommended schools page

--- EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/recommended_insurance.java
+++ EasyLearnDrivingLessons/app/src/main/java/com/example/easylearndrivinglessons/recommended_insurance.java
@@ -12,18 +12,10 @@ public class recommended_insurance extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reccomended_insurance);
+        setContentView(R.layout.activity_recommended_insurance);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
